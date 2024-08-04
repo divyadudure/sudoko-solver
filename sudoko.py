459d0b4782627431c7d9df29f4b1faf270967e4e
divyadudure <divyadudure@gmail.com>
2024-08-04 16:00:21 +0530

    Add files via upload

---
+++ sudoko.py
@@ -0,0 +1,60 @@
+def is_valid(board, row, col, num):
+    # Check if num is not in the current row, column, or 3x3 sub-grid
+    for x in range(9):
+        if board[row][x] == num or board[x][col] == num:
+            return False
+
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    empty = find_empty(board)
+    if not empty:
+        return True  # Puzzle solved
+
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0  # Backtrack
+
+    return False
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) for num in row))
+
+# Example usage
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(sudoku_board):
+    print_board(sudoku_board)
+else:
+    print("No solution exists")
